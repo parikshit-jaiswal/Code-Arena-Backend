9488a16d5a24027b3222d3a133e56469b0fe6be9
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2025-04-24 19:11:40 +0530

    Add OTP email sending functionality and update dependencies

--- src/controllers/test.controllers.ts
+++ src/controllers/test.controllers.ts
@@ -2,9 +2,14 @@ import { Request, Response } from "express";
 import axios from "axios";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { sendOtpEmail } from "../utils/sendMail.js";
 
 const test = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json(new ApiResponse(200, { message: "Test route is working" }, "Test route is working"));
+    const { email }: { email: string } = { email: "parikshitjaiswal82@gmail.com" };
+
+    const otp: number = Math.floor(100000 + Math.random() * 900000);
+    await sendOtpEmail(email, otp.toString());
+    res.status(200).json(new ApiResponse(200, { message: "Test route is working", otp }, "Test route is working"));
 });
 
 export { test };
