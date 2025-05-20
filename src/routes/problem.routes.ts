7876accc40a7232a93430d1cedb67e2c1ca90415
vansh mac <vansh23164033@akgec.ac.in>
2025-05-20 18:01:20 +0530

    feat: enhance problem submission and retrieval functionality with improved error handling and code organization

--- src/routes/problem.routes.ts
+++ src/routes/problem.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { submitSolution } from "../controllers/problem.controllers.js";
+import { getProblem, submitSolution } from "../controllers/problem.controllers.js";
+import { getContestProblems } from "../controllers/contest.controllers.js";
 
 const router = Router();
 
 router.route('/submit-solution/:contestId/:problemId').post(verifyJWT, submitSolution);
-
+router.route('/get-problem/:contestId/:problemId').get(verifyJWT, getProblem);
 export default router
